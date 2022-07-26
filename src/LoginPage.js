1bc5ef6b41055dbe7ae1f9faf94de4d1177a71d4
farzinsaniya <saniyafarzin75@gmail.com>
2022-07-26 20:41:44 +0530

    added mui

---
+++ src/LoginPage.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+
+export default function BoxComponent() {
+  return (
+    <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
+      
+    </Box>
+  );
+}
